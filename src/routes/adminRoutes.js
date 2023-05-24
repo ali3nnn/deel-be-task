a84819a8c14eec80ca8cc9b6053e505f586753f8
Alex Barbu <barbualex.tgv@gmail.com>
2023-05-24 13:06:20 +0300

    feat: added admin routes

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const AdminService = require('../services/adminService');
+
+// GET /admin/best-profession - Get the best profession within a date range
+router.get('/best-profession', async (req, res) => {
+    const models = req.app.get('models');
+    const adminService = new AdminService(models);
+    
+    const startDate = new Date(req.query.start);
+    const endDate = new Date(req.query.end);
+
+    try {
+        const bestProfession = await adminService.getBestProfession(startDate, endDate, req.app.get('models'));
+        res.status(200).send(bestProfession);
+    } catch (error) {
+        res.status(error.status || 500).send(error.message);
+    }
+});
+
+// GET /admin/best-clients - Get the best clients within a date range
+router.get('/best-clients', async (req, res) => {
+    const models = req.app.get('models');
+    const adminService = new AdminService(models);
+
+    const startDate = new Date(req.query.start);
+    const endDate = new Date(req.query.end);
+    const limit = req.query.limit;
+
+    try {
+        const bestClients = await adminService.getBestClients(startDate, endDate, limit, req.app.get('models'));
+        res.status(200).send(bestClients);
+    } catch (error) {
+        res.status(error.status || 500).send(error.message);
+    }
+});
+
+module.exports = router;
+
