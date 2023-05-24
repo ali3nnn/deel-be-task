1772b9c6244ab92b8a13019e047458afa419fdaa
Alex Barbu <barbualex.tgv@gmail.com>
2023-05-24 13:06:11 +0300

    feat: added job service class

---
+++ src/services/JobService.js
@@ -0,0 +1,139 @@
+const { Op, Sequelize } = require('sequelize');
+const { sequelize } = require('../model');
+
+class JobService {
+  constructor(models) {
+    this.Job = models.Job;
+    this.Contract = models.Contract;
+    this.Profile = models.Profile;
+    this.sequelize = models.sequelize;
+  }
+
+  async getUnpaidJobsForUser(profileId) {
+
+    const jobs = await this.Job.findAll({
+      include: {
+        model: this.Contract,
+        as: 'Contract',
+        where: {
+          status: 'in_progress',
+          [Op.or]: [
+            { ContractorId: profileId },
+            { ClientId: profileId },
+          ],
+        },
+      },
+      where: { paid: { [Op.or]: [null, false] } },
+    });
+
+    return jobs;
+  }
+
+  async payJob(jobId, client) {
+    const t = await sequelize.transaction();
+
+
+    if (client.type === 'contractor') {
+      throw {
+        message: 'You are not a client',
+        status: 403
+      }
+    }
+
+    const job = await this.Job.findOne({
+      include: {
+        model: this.Contract,
+        as: 'Contract',
+        where: {
+          status: 'in_progress',
+          ClientId: client.id
+        }
+      },
+      where: {
+        id: jobId,
+        [Op.or]: [
+          { paid: null },
+          { paid: false }
+        ]
+      }
+    })
+
+    if (!job) {
+      throw {
+        message: "Job not found or it has been paid",
+        status: 200
+      }
+    }
+
+    if (client.balance < job.price) {
+      throw {
+        message: "Client doesn't have enough balance",
+        status: 200
+      }
+    }
+
+    const contractor = await this.Profile.findOne({
+      where: {
+        id: job.Contract.ContractorId
+      }
+    })
+
+    try {
+
+      // Update the job as paid
+      const p1 = await this.Job.update({
+        paid: true,
+        paymentDate: new Date()
+      }, {
+        where: {
+          id: jobId
+        },
+        transaction: t
+      })
+
+      // Update the contractor's balance
+      const p2 = await this.Profile.update({
+        balance: contractor.balance + job.price
+      }, {
+        where: {
+          id: job.Contract.ContractorId
+        },
+        transaction: t
+      })
+
+      // Update the client's balance
+      const p3 = await this.Profile.update({
+        balance: client.balance - job.price
+      }, {
+        where: {
+          id: client.id
+        },
+        transaction: t
+      })
+
+      const transactions = await Promise.all([p1, p2, p3])
+      const isUpdatedFailed = transactions.some(item => !item[0])
+
+      if(isUpdatedFailed) {
+        throw {
+          message: "Request failed",
+          status: 500
+        }
+      }
+
+      // Run the transaction
+      await t.commit()
+
+      return true;
+    } catch (error) {
+
+      // Rollback the transaction
+      await t.rollback();
+
+      throw error;
+    }
+  }
+
+}
+
+module.exports = JobService;
